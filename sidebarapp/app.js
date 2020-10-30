5e1cc821fc4b47ff058cc83aeb2aeed726af5f82
Güney Ural <34598370+urlGuney@users.noreply.github.com>
2020-10-30 22:47:05 +0300

    add responsivity

--- sidebarapp/app.js
+++ sidebarapp/app.js
@@ -3,9 +3,21 @@ const btn = document.querySelector('#hamburger');
 const closeBtn = document.querySelector('.close-button');
 
 btn.addEventListener('click', () => {
-     sidenav.classList.toggle('visible');
+     if(window.innerWidth <= 800) {
+          sidenav.classList.toggle('visible');
+          btn.classList.toggle('hidden');
+          sidenav.style.width = "100%";
+     } else {
+          sidenav.classList.toggle('visible');
+     }
 });
 
 closeBtn.addEventListener('click', ()=> {
-     sidenav.classList.toggle('visible');
+     if(window.innerWidth <= 800) {
+          sidenav.classList.toggle('visible');
+          btn.classList.toggle('hidden');
+          sidenav.style.width = "350px";
+     } else{
+          sidenav.classList.toggle('visible');
+     }
 });
